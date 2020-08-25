88671cef0fe001bf5dd377d6004ba2320f191c61
zhye78 <ccoje2155@naver.com>
2020-08-25 22:33:51 +0900

    custom login form + login error처리 기능 추가

---
+++ src/main/java/kr/ac/hansung/controller/LoginController.java
@@ -0,0 +1,20 @@
+package kr.ac.hansung.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public String showLogin(@RequestParam(value="error", required=false) String error, Model model) {
+		
+		if(error != null) {
+			model.addAttribute("errorMsg", "Invalid username and password");
+		}
+		
+		return "login";
+	}
+}
